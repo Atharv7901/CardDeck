90e63076412ecdb6468ea8950cf91d36e378c9e2
Atharv7901 <atharvshivarkar@gmail.com>
2024-08-10 16:28:23 +0530

    testing io

--- dek_test.go
+++ dek_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 func TestNewDeck(t *testing.T) {
 	d := newDeck()
@@ -16,4 +19,19 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "Three of Clubs" {
 		t.Errorf("Expected Three of Clubs, got %v", d[len(d)-1])
 	}
+}
+
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T){
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+
+	loadedDeck := newDeckfromFile("_decktesting")
+
+	if len(loadedDeck) != 16 {
+		t.Errorf("Expected 16, got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
